a84af575520caa45e2a90807b2eb3288517fce64
vinjas <danielvinyastorres@gmail.com>
2021-05-06 11:31:08 +0200

    update componente to be able to introduce numbers

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,10 +9,34 @@ class Calculator extends React.Component {
             result: 0,
             input: 0,
         }
+        this.addNumber = this.addNumber.bind(this)
+        this.pressC = this.pressC.bind(this)
     }
+    
     componentDidMount() {
         document.body.style.backgroundColor = "#306182"
     }
+
+    addNumber(number) {  
+        if (this.state.input === 0) {
+            return this.setState({
+                input: parseInt("" + number, 10)
+            })
+        } else {
+            return this.setState({
+                input: parseInt("" + this.state.input + number, 10)
+            })      
+        }
+    }
+
+
+    pressC() {
+        this.setState({
+            result: 0,
+            input: 0,
+        })
+    }
+
 
     render() {
         return (
@@ -29,7 +53,8 @@ class Calculator extends React.Component {
                         id="F">F</div>
                         <div
                         className="calc-key equal"
-                        id="C">C</div>
+                        id="C"
+                        onClick={this.pressC}>C</div>
                         <div
                         className="calc-key"
                         id="divide">/</div>
@@ -38,52 +63,69 @@ class Calculator extends React.Component {
                         id="multiple">*</div>
                         <div
                         className="calc-key numeral"
-                        id="7">7</div>
+                        id="7"
+                        onClick={() => this.addNumber(7)}>7</div>
                         <div
                         className="calc-key numeral"
-                        id="8">8</div>
+                        id="8"
+                        number={8}
+                        onClick={() => this.addNumber(8)}>8</div>
                         <div
                         className="calc-key numeral"
-                        id="9">9</div>
+                        id="9"
+                        number={9}
+                        onClick={() => this.addNumber(9)}>9</div>
                         <div
                         className="calc-key"
                         id="minus">-</div>
                         <div
                         className="calc-key numeral"
-                        id="4">4</div>
+                        id="4"
+                        number={4}
+                        onClick={() => this.addNumber(4)}>4</div>
                         <div
                         className="calc-key numeral"
-                        id="5">5</div>
+                        id="5"
+                        number={5}
+                        onClick={() => this.addNumber(5)}>5</div>
                         <div
                         className="calc-key numeral"
-                        id="6">6</div>
+                        id="6"
+                        number={6}
+                        onClick={() => this.addNumber(6)}>6</div>
                         <div
                         className="calc-key"
                         od="plus">+</div>
                         <div
                         className="calc-key numeral"
-                        id="1">1</div>
+                        id="1"
+                        number={1}
+                        onClick={() => this.addNumber(1)}>1</div>
                         <div
                         className="calc-key numeral"
-                        id="2">2</div>
+                        id="2"
+                        number={2}
+                        onClick={() => this.addNumber(2)}>2</div>
                         <div
                         className="calc-key numeral"
-                        id="3">3</div>
+                        id="3"
+                        number={3}
+                        onClick={() => this.addNumber(3)}>3</div>
                         <div
                         className="calc-key equal"
                         id="equal">=</div>
                         <div
                         className="calc-key numeral"
                         id="0">0</div>
-                        <div
+                        {/*<div
                         className="calc-key numeral"
-                        id="0">0</div>
+                        id="0">0</div>*/}
                         <div
                         className="calc-key coma"
                         id="decimal">,</div>
-                        <div
+                        {/*<div
                         className="calc-key equal"
-                        id="equal">=</div>
+                        id="equal">=</div>*/}
                 </div>
             </div>
         )
